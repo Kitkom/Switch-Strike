f492329a661f612798879775850ce2b0fce19010
Kitkom Xu <kitkom.xu@gmail.com>
2017-05-27 22:40:53 +0800

    Transfer 0527

---
+++ client/Assets/Scripts/HistoryListLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HistoryListLoader : MonoBehaviour
+{
+	public Dropdown historyList;
+	private List<Dropdown.OptionData> list;
+	// Use this for initialization
+	void Start()
+	{
+		list = historyList.options;
+		list.Clear ();
+		list.Add (new Dropdown.OptionData ("1.Alpha"));
+		list.Add (new Dropdown.OptionData ("2.Beta"));
+		list.Add (new Dropdown.OptionData ("3.Gamma"));
+		list.Add (new Dropdown.OptionData ("4.Alpha"));
+		list.Add (new Dropdown.OptionData ("5.Beta"));
+		list.Add (new Dropdown.OptionData ("6.Gamma"));
+		list.Add (new Dropdown.OptionData ("7.Alpha"));
+		list.Add (new Dropdown.OptionData ("8.Beta"));
+		list.Add (new Dropdown.OptionData ("9.Gamma"));
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ client/Assets/Scripts/PlayerListLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerListLoader : MonoBehaviour
+{
+
+	public Dropdown playerList;
+	private List<Dropdown.OptionData> list;
+	// Use this for initialization
+	void Start()
+	{
+		list = playerList.options;
+		list.Clear();
+		list.Add(new Dropdown.OptionData("1.Alpha"));
+		list.Add(new Dropdown.OptionData("2.Beta"));
+		list.Add(new Dropdown.OptionData("3.Gamma"));
+		list.Add(new Dropdown.OptionData("4.Alpha"));
+		list.Add(new Dropdown.OptionData("5.Beta"));
+		list.Add(new Dropdown.OptionData("6.Gamma"));
+		list.Add(new Dropdown.OptionData("7.Alpha"));
+		list.Add(new Dropdown.OptionData("8.Beta"));
+		list.Add(new Dropdown.OptionData("9.Gamma"));
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
